7d70aa139812b91201d6dd71b2dafcbdf0025df5
Christian Wirth <christian-wirth@mailbox.org>
2018-03-24 11:37:29 +0100

    Abhängikeiten

--- app/src/main/java/com/example/simon/instantmessengerapp/core/rest/services/GroupRestClientImpl.java
+++ app/src/main/java/com/example/simon/instantmessengerapp/core/rest/services/GroupRestClientImpl.java
@@ -18,6 +18,9 @@ import java.util.ArrayList;
 //import javax.ws.rs.client.ClientBuilder;
 //import javax.ws.rs.client.Entity;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
--- app/src/main/java/com/example/simon/instantmessengerapp/core/rest/services/UserRestClientImpl.java
+++ app/src/main/java/com/example/simon/instantmessengerapp/core/rest/services/UserRestClientImpl.java
@@ -15,6 +15,9 @@ import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
